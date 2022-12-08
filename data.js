f9ac6ad042792b33dbdccea5c2527658ed464914
mgdeveloper95@gmail.com <mgdeveloper95@gmail.com>
2022-12-08 12:49:48 +0000

    display all products

--- data.js
+++ data.js
@@ -1,74 +1,64 @@
-import IMG1 from './images/1.jpeg';
-import IMG2 from './images/2.jpeg';
-import IMG3 from './images/3.jpeg';
-import IMG4 from './images/4.jpeg';
-import IMG5 from './images/5.jpeg';
-import IMG6 from './images/6.jpeg';
-import IMG7 from './images/7.jpeg';
-import IMG8 from './images/8.jpeg';
-import IMG9 from './images/9.jpeg';
-
 export const data = [
 	{
 		id: 1,
 		name: 'SUIS POLTRONA MENTA',
-		img: IMG1,
+		img: './images/1.jpeg',
 		price: 249,
 		cat: 'POLTRONAS',
 	},
 	{
 		id: 2,
 		name: 'OEUF POLTRONA PÉROLA',
-		img: IMG2,
+		img: './images/2.jpeg',
 		price: 419,
 		cat: 'POLTRONAS',
 	},
 	{
 		id: 3,
 		name: 'JUNE SOFÁ COM ARMAZENAMENTO',
-		img: IMG3,
+		img: './images/3.jpeg',
 		price: 1159,
 		cat: 'CHAISE LONGUE',
 	},
 	{
 		id: 4,
 		name: 'EVRON CABECEIRA 150',
-		img: IMG4,
+		img: './images/4.jpeg',
 		price: 149,
 		cat: 'CABECEIRAS',
 	},
 	{
 		id: 5,
 		name: 'LOEN ESPELHO',
-		img: IMG5,
+		img: './images/5.jpeg',
 		price: 269,
 		cat: 'ESPELHOS',
 	},
 	{
 		id: 6,
 		name: 'MOEBE ESPELHO DOURADO 90X90',
-		img: IMG6,
+		img: './images/6.jpeg',
 		price: 199,
 		cat: 'ESPELHOS',
 	},
 	{
 		id: 7,
 		name: 'ZAMIOCULCAS PLANTA ARTIFICIAL',
-		img: IMG7,
+		img: './images/7.jpeg',
 		price: 249,
 		cat: 'PLANTAS ARTIFICIAIS',
 	},
 	{
 		id: 8,
 		name: 'BANANEIRA PLANTA ARTIFICIAL 135 CM',
-		img: IMG8,
+		img: './images/8.jpeg',
 		price: 169,
 		cat: 'PLANTAS ARTIFICIAIS',
 	},
 	{
 		id: 9,
 		name: 'VINTI APARADOR CARVALHO 165',
-		img: IMG9,
+		img: './images/9.jpeg',
 		price: 869,
 		cat: 'APARADORES',
 	},
--- index.js
+++ index.js
@@ -2,11 +2,33 @@
 import { data } from './data.js';
 
 // DOM ACCESS
-const searchInput = document.getElementById('search');
 const productsContainer = document.getElementById('products');
+const searchInput = document.getElementById('search');
 const categoriesContainer = document.getElementById('categories');
 const priceRange = document.getElementById('priceRange');
 const priceAmount = document.getElementById('priceAmount');
+
+// DISPLAY ALL EXISTING PRODUCTS
+
+const displayProducts = (filteredProducts) => {
+	productsContainer.innerHTML = filteredProducts
+		.map(
+			(product) =>
+				`
+       <div class="single-product">
+          <img
+          src=${product.img}
+          alt="${product.name}"
+          />
+          <p class="product-name">${product.name}</p>
+          <p class="product-price">${product.price} €</p>
+        </div>
+    `
+		)
+		.join('');
+};
+
+displayProducts(data);
 
 // SEARCH INPUT
 

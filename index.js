cf022a44a8da356110c39d6559c77d90c2bf7baa
mgdeveloper95@gmail.com <mgdeveloper95@gmail.com>
2022-12-08 12:30:59 +0000

    grab by ID the elements to manipulate using DOM

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+/* import JSON object */
+import { data } from './data.js';
 
+// DOM ACCESS
+const searchInput = document.getElementById('search');
+const productsContainer = document.getElementById('products');
+const categoriesContainer = document.getElementById('categories');
+const priceRange = document.getElementById('priceRange');
+const priceAmount = document.getElementById('priceAmount');
+
+// SEARCH INPUT
+
+// DISPLAY PRODUCT CATEGORIES
+
+// PRICE RANGE FILTER
